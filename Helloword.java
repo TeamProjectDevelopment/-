b2a0702dac4d1b6cfb1a2f7030df288522e592b5
omwang <164437670@qq.com>
2018-03-22 10:30:22 +0800

    try

--- Helloword.java
+++ Helloword.java
@@ -3,7 +3,7 @@ public class Helloword {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Helloworld by wang online");
+		System.out.println("Helloworld by wang");
 
 	}
 }
